ba96d920fdc9a8552c7002e65d2b6b0cd5e4c663
Nicolas Guillemot <nlguillemot@gmail.com>
2017-05-18 16:07:51 -0700

    refactor globals

--- webgl-template.js
+++ webgl-template.js
@@ -1,20 +1,15 @@
-// globals
-var gTriangleVertexBuffer;
-var gTriangleIndexBuffer;
-var gShaderProgram;
-
 function onload () {
     // initialize GL
     var canvas = document.getElementById("glcanvas");
     var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 
     // do one-time initialization of graphics resources
-    init(gl);
+    var scene = init(gl);
 
     // the callback for each frame of animation (ie. at ~60fps)
     function animationCallback(timestamp) {
         // draw this frame
-        draw(gl, timestamp);
+        draw(gl, scene, timestamp);
         
         // request the next frame of animation
         window.requestAnimationFrame(animationCallback);
@@ -55,13 +50,13 @@ function init (gl) {
                                "}\n";
 
     // allocate and upload vertex data
-    gTriangleVertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, gTriangleVertexBuffer);
+    var triangleVertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, triangleVertexData, gl.STATIC_DRAW);
 
     // allocate and upload index data
-    gTriangleIndexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gTriangleIndexBuffer);
+    var triangleIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, triangleIndexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, triangleIndexData, gl.STATIC_DRAW);
 
     // upload and compile the vertex shader
@@ -75,34 +70,41 @@ function init (gl) {
     gl.compileShader(fragmentShader);
 
     // link the vertex shader and fragment shader together into a shader program
-    gShaderProgram = gl.createProgram();
-    gl.attachShader(gShaderProgram, vertexShader);
-    gl.attachShader(gShaderProgram, fragmentShader);
-    gl.linkProgram(gShaderProgram);
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    
+    // return the "scene" data that will be passed to draw()
+    return {
+        triangleVertexBuffer: triangleVertexBuffer,
+        triangleIndexBuffer: triangleIndexBuffer,
+        shaderProgram: shaderProgram
+    };
 }
 
-function draw (gl, timestamp) {   
+function draw (gl, scene, timestamp) {   
     // clear the screen
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     // set the shader program to use to draw
-    gl.useProgram(gShaderProgram);
+    gl.useProgram(scene.shaderProgram);
 
     // update the uniform tint
     var currentTimeInSeconds = Date.now() / 1000.0;
     var strobePeriodInSeconds = 3;
     var tint = Math.abs(Math.sin(Math.PI / strobePeriodInSeconds * currentTimeInSeconds));
-    var tintLocation = gl.getUniformLocation(gShaderProgram, "tint");
+    var tintLocation = gl.getUniformLocation(scene.shaderProgram, "tint");
     gl.uniform4f(tintLocation, tint, 0.0, 0.0, 1.0);
 
     // explain to GL how to read the raw position data that was uploaded, then enable it.
-    var positionLocation = gl.getAttribLocation(gShaderProgram, "vertexPosition");
+    var positionLocation = gl.getAttribLocation(scene.shaderProgram, "vertexPosition");
     var positionSize = 2;
     var positionType = gl.FLOAT;
     var positionNormalized = false;
     var positionStrideInBytes = 20;
     var firstPositionOffsetInBytes = 0;
-    gl.bindBuffer(gl.ARRAY_BUFFER, gTriangleVertexBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, scene.triangleVertexBuffer);
     gl.vertexAttribPointer(
         positionLocation,
         positionSize,
@@ -113,13 +115,13 @@ function draw (gl, timestamp) {
     gl.enableVertexAttribArray(positionLocation);
 
     // explain to GL how to read the raw color data that was uploaded, then enable it.
-    var colorLocation = gl.getAttribLocation(gShaderProgram, "vertexColor");
+    var colorLocation = gl.getAttribLocation(scene.shaderProgram, "vertexColor");
     var colorSize = 3;
     var colorType = gl.FLOAT;
     var colorNormalized = false;
     var colorStrideInBytes = 20;
     var firstColorOffsetInBytes = 8;
-    gl.bindBuffer(gl.ARRAY_BUFFER, gTriangleVertexBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, scene.triangleVertexBuffer);
     gl.vertexAttribPointer(
         colorLocation,
         colorSize,
@@ -133,6 +135,6 @@ function draw (gl, timestamp) {
     var indexCount = 3;
     var indexType = gl.UNSIGNED_SHORT;
     var firstIndexOffsetInBytes = 0;
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gTriangleIndexBuffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, scene.triangleIndexBuffer);
     gl.drawElements(gl.TRIANGLES, indexCount, indexType, firstIndexOffsetInBytes);
 }
